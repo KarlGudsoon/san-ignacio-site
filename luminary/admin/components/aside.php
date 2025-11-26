ee4adce6559b0cdbd827930a5d7de976021f2b6a
KarlGudsoon <maturana.or.adrian@gmail.com>
2025-11-26 16:23:10 -0300

    cambios varios

--- luminary/admin/components/aside.php
+++ luminary/admin/components/aside.php
@@ -2,10 +2,10 @@
     <nav>
         <ul>
             <li style="background: white;"><img class="icon" src="/assets/img/logo.svg" alt=""></li>
-            <li><a href="admin.php"><img class="icon" src="/assets/icons/home.svg"></a></li>
-            <li><a href="admin_cursos.php"><img class="icon" src="/assets/icons/fa6-solid--list-ol.svg"></a></li>
-            <li><a href="admin_profesores.php"><img class="icon" src="/assets/icons/teacher.svg"></a></li>    
-            <li><a href="matriculas.php"><img class="icon" src="/assets/icons/teacher.svg"></a></li>    
+            <li class="seleccionada"><a href="admin.php"><img class="icon" src="/assets/icons/home.svg">Inicio</a></li>
+            <li><a href="admin_cursos.php"><img class="icon" src="/assets/icons/fa6-solid--list-ol.svg">Cursos</a></li>
+            <li><a href="admin_profesores.php"><img class="icon" src="/assets/icons/teacher.svg">Profesores</a></li>     
+            <li><a href="matriculas.php"><img class="icon" src="/assets/icons/school.svg">Matrículas</a></li>   
         </ul>
         <a href="../logout.php"><img class="icon" src="/assets/icons/tabler--logout.svg"></a>
     </nav>

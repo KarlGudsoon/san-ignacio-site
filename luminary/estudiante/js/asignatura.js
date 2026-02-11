b21963d2b9c45e16c5ce6e66cbc148be5d8724ad
KarlGudsoon <maturana.or.adrian@gmail.com>
2026-02-11 14:22:06 -0300

    Cambio en ui de notas de asignatura

--- luminary/estudiante/js/asignatura.js
+++ luminary/estudiante/js/asignatura.js
@@ -9,6 +9,42 @@ async function initAsignaturaDetalle(asignaturaId) {
   const iconGuardado = sessionStorage.getItem("asignaturaIcon");
   asignaturaIcon.src = iconGuardado;
 
+  const asignaturaWidget = document.getElementById("asignatura-widget");
+
+  async function cargarWidgets(asignaturaId) {
+    return fetch(
+      `/luminary/api/estudiante/notas_asignatura.php?asignatura_id=${asignaturaId}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const notas = data[0];
+        const contenedorPromedio = document.createElement("div");
+        asignaturaWidget.appendChild(contenedorPromedio);
+        contenedorPromedio.classList.add("contenedor-promedio");
+        contenedorPromedio.innerHTML = `
+                <div class="promedio" style="background-color:${colorGuardado};">
+                  <div class="header">
+                   <span>Promedio asignatura</span>
+                  </div>
+                  <div class="promedio-porcentaje">
+                    <svg viewBox="0 0 120 120">
+                      <!-- fondo -->
+                      <circle class="ring-bg" cx="60" cy="60" r="54" />
+                      <!-- progreso -->
+                      <circle class="ring-progress" cx="60" cy="60" r="54" />
+                    </svg>
+                    <div class="promedio-nota">${notas.x̄}</div>
+                  </div>
+                  <p id="progress-text"></p>
+                  <span style="color: rgba(255,255,255,0.75); font-size: 0.875rem;">RENDIMIENTO ACADÉMICO</span>
+                </div>
+                <div class="promedio"></div>
+              `;
+        setProgress(((notas.x̄ / 7) * 100).toFixed(0));
+      });
+  }
+  cargarWidgets(asignaturaId);
+
   document.getElementById("volver").addEventListener("click", () => {
     cargarView("asignaturas");
   });
@@ -38,40 +74,53 @@ async function initAsignaturaDetalle(asignaturaId) {
   }
 
   async function asignaturaNotas() {
-    return fetch(`/luminary/api/estudiante/notas?asignatura_id=${asignaturaId}`)
+    return fetch(
+      `/luminary/api/estudiante/notas_asignatura.php?asignatura_id=${asignaturaId}`,
+    )
       .then((res) => res.json())
       .then((data) => {
         const contenedor = document.getElementById("asignatura-contenido");
+        contenedor.className = "asignatura-contenido-notas";
 
         if (!data.length) {
           contenedor.innerHTML = "<p>Sin notas registradas</p>";
           return;
         }
 
+        contenedor.innerHTML = "";
+
         const notas = data[0];
 
-        let html = "";
+        let contenedorNotas = document.createElement("div");
+        contenedorNotas.classList.add("contenedor-notas");
 
         for (let i = 1; i <= 9; i++) {
           const valor = notas[`nota${i}`];
           if (valor == null) continue;
+          let colorNota;
+          if (valor < 4) {
+            colorNota = "#e24a4a";
+          } else if (valor >= 4) {
+            colorNota = "#2589df";
+          }
 
-          html += `
-          <div class="nota-item">
-            <span>Nota ${i}</span>
-            <strong>${valor}</strong>
-          </div>
-        `;
+          const nota = document.createElement("div");
+          nota.classList.add("nota-item");
+          nota.innerHTML = `
+            <div style="display: flex; flex-direction: column; justify-content: center;">
+              <h4 style="margin: 0;">Nota ${i}</h4>
+              <div style="display: flex; gap: 0.5rem;">
+                <span>Fecha de evaluación</span> 
+                <span>Tipo de evaluación</span> 
+              </div>
+            </div>
+            <div class="nota" style="background-color: ${colorNota};">${valor}</div>
+            `;
+
+          contenedorNotas.appendChild(nota);
         }
 
-        html += `
-          <div class="nota-item">
-            <span>Promedio</span>
-            <strong>${notas.x̄}</strong>
-          </div>
-          `;
-
-        contenedor.innerHTML = html || "<p>Sin notas</p>";
+        contenedor.appendChild(contenedorNotas);
       });
   }
 
@@ -92,4 +141,36 @@ async function initAsignaturaDetalle(asignaturaId) {
       document.getElementById("profesor").textContent =
         "Profesor: " + data.profesor;
     });
+}
+
+function setProgress(percent) {
+  const circle = document.querySelector(".ring-progress");
+  const text = document.getElementById("progress-text");
+
+  const radius = 54;
+  const circumference = 2 * Math.PI * radius;
+
+  let color;
+  if (percent >= 71) {
+    color = "#0da761";
+  } else if (percent >= 57) {
+    color = "#f2a400"; // Amarillo
+  } else {
+    color = "#eb3b3b"; // Rojo
+  }
+  circle.style.stroke = color;
+  circle.style.strokeDasharray = circumference;
+  setTimeout(() => {
+    circle.style.strokeDashoffset =
+      circumference - (percent / 100) * circumference;
+  }, 50);
+  if (percent >= 85) {
+    text.textContent = "EXCELENTE";
+  } else if (percent >= 71) {
+    text.textContent = "BIEN";
+  } else if (percent >= 57) {
+    text.textContent = "REGULAR";
+  } else {
+    text.textContent = "NECESITA MEJORAR";
+  }
 }

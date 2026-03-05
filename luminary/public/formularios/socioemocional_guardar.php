60b9f2d01e87a3d587e7517aa0be5f1e36867e22
KarlGudsoon <maturana.or.adrian@gmail.com>
2026-03-05 15:00:22 -0300

    Update socioemocional_guardar.php

--- luminary/public/formularios/socioemocional_guardar.php
+++ luminary/public/formularios/socioemocional_guardar.php
@@ -43,7 +43,7 @@ $sql = "INSERT INTO apoyo_socioemocional (
 
 $stmt = $conexion->prepare($sql);
 $stmt->bind_param(
-    "sssssis",
+    "sssssss",
     $nombre_estudiante,
     $apellidos_estudiante,
     $rut_estudiante,

83fa69570fd2f8c9e6b70d7d64ff8428acdb0a5a
KarlGudsoon <maturana.or.adrian@gmail.com>
2026-07-06 21:11:49 -0400

    Update estudiante_traspasar.php

--- luminary/api/admin/estudiantes/estudiante_traspasar.php
+++ luminary/api/admin/estudiantes/estudiante_traspasar.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../../middlewares/auth_admin2.php';
 require_once __DIR__ . "/../../config/db.php";
+require_once __DIR__ . '/../../middlewares/auth_admin2.php';
 header("Content-Type: application/json");
 
 // Validar sesión
